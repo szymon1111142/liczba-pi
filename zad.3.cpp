3f37386a50895209178e357f989a8514edb609d3
szymguzda <59117149+szymon1111142@users.noreply.github.com>
2023-02-22 14:42:43 +0100

    Add files via upload

---
+++ zad.3.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+  const double PI = 3.14 ;
+  double root = sqrt(PI);
+
+  std::cout << std::fixed << std::setprecision(2) << root << std::endl;
+
+  return 0;
+    
+}
